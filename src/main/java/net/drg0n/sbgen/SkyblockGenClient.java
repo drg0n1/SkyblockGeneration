eba561425c4b0190a8cd35cb3671e4af0cbe1510
drg0n <adrien.bernard2005@gmail.com>
2024-12-28 21:10:30 +0100

    Mod template creation

---
+++ src/main/java/net/drg0n/sbgen/SkyblockGenClient.java
@@ -0,0 +1,10 @@
+package net.drg0n.sbgen;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkyblockGenClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/drg0n/sbgen/SkyblockGenDataGenerator.java
@@ -0,0 +1,11 @@
+package net.drg0n.sbgen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SkyblockGenDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
